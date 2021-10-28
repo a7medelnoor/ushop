493524b770d9ad0f615cf3a70e135b9c93827e45
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-28 18:03:01 +0400

    setup navigation button based on the destination

--- app/src/main/java/com/a7medelnoor/ushop/MainActivity.kt
+++ app/src/main/java/com/a7medelnoor/ushop/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.a7medelnoor.ushop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       setupNav()
     }
+
+    private fun setupNav() {
+        val navController = findNavController(R.id.navHostFragment)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigation.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigation.visibility = View.GONE
+    }
+
 }

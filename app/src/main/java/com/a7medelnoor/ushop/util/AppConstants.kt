4415a9dff390f73fd50f3f86ad72c0d71cc335dd
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-28 18:03:20 +0400

    add user login url constant

--- app/src/main/java/com/a7medelnoor/ushop/util/AppConstants.kt
+++ app/src/main/java/com/a7medelnoor/ushop/util/AppConstants.kt
@@ -3,5 +3,6 @@ package com.a7medelnoor.ushop.util
 class AppConstants {
     companion object{
         const val BASE_URL = "https://ushop.ae/endpoints/v1/"
+        const val LOGIN_URL ="auth/login"
     }
 }

22c8173c6a9a01385c1bfb7f80bcfcf3c8dc87ba
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-28 13:23:29 +0400

    Initial commit

---
+++ app/src/main/java/com/a7medelnoor/ushop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a7medelnoor.ushop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

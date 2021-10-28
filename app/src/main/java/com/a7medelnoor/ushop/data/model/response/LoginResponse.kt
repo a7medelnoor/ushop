bf918ab316ecd428bc0170bc56e3725e7e557ab0
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-28 18:05:16 +0400

    add serialization to our params

--- app/src/main/java/com/a7medelnoor/ushop/data/model/response/LoginResponse.kt
+++ app/src/main/java/com/a7medelnoor/ushop/data/model/response/LoginResponse.kt
@@ -1,7 +1,12 @@
 package com.a7medelnoor.ushop.data.model.response
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse (
+    @SerializedName("code")
     val code: Int,
+    @SerializedName("status")
     val status: String,
-    val dataResponse: UserDataResponse
+    @SerializedName("data")
+    val data: UserDataResponse
         )

cfd9f38d7d55f8820a2380294487780728954cca
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-30 15:17:05 +0400

    add categories method to our services

--- app/src/main/java/com/a7medelnoor/ushop/data/network/UShopApiService.kt
+++ app/src/main/java/com/a7medelnoor/ushop/data/network/UShopApiService.kt
@@ -1,7 +1,11 @@
 package com.a7medelnoor.ushop.data.network
 
+import com.a7medelnoor.ushop.data.model.response.CategoriesData
+import com.a7medelnoor.ushop.data.model.response.CategoriesResponse
 import com.a7medelnoor.ushop.data.model.response.LoginResponse
+import com.a7medelnoor.ushop.util.AppConstants.Companion.CATEGORIES_URL
 import com.a7medelnoor.ushop.util.AppConstants.Companion.LOGIN_URL
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Headers
@@ -12,9 +16,16 @@ interface UShopApiService {
     @POST(LOGIN_URL)
     @Headers("Content-Type:application/x-www-form-urlencoded")
     @FormUrlEncoded
-    fun login(
+    fun userLogin(
         @Field("server_key") server_key: String,
         @Field("username") username: String,
         @Field("password") password: String
     ): retrofit2.Call<LoginResponse>
+    @POST(CATEGORIES_URL)
+    @Headers("Content-Type:application/x-www-form-urlencoded")
+    @FormUrlEncoded
+    fun getCategories(
+        @Field("server_key") server_key: String,
+        @Field("access_token") access_token: String
+    ): Call<CategoriesResponse>
 }

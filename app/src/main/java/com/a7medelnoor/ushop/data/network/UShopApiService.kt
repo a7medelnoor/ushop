c22c439ec04003da919926dec489ccd8ffb28421
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-30 13:05:49 +0400

    configure our api service interface with login method

--- app/src/main/java/com/a7medelnoor/ushop/data/network/UShopApiService.kt
+++ app/src/main/java/com/a7medelnoor/ushop/data/network/UShopApiService.kt
@@ -1,16 +1,20 @@
 package com.a7medelnoor.ushop.data.network
 
-import android.telecom.Call
-import com.a7medelnoor.ushop.data.model.request.LoginRequest
 import com.a7medelnoor.ushop.data.model.response.LoginResponse
 import com.a7medelnoor.ushop.util.AppConstants.Companion.LOGIN_URL
-import retrofit2.http.Body
+import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface UShopApiService {
 
     @POST(LOGIN_URL)
+    @Headers("Content-Type:application/x-www-form-urlencoded")
     @FormUrlEncoded
-    fun login(@Body request: LoginRequest): retrofit2.Call<LoginResponse>
+    fun login(
+        @Field("server_key") server_key: String,
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): retrofit2.Call<LoginResponse>
 }

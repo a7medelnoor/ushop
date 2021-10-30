55faddf09a5260c6886d4234754dc7ca1c6d8fb3
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-30 13:08:25 +0400

    create all categories fragment

---
+++ app/src/main/java/com/a7medelnoor/ushop/ui/allCategories/AllCategoriesFragment.kt
@@ -0,0 +1,24 @@
+package com.a7medelnoor.ushop.ui.allCategories
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.a7medelnoor.ushop.R
+
+class AllCategoriesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_categories, container, false)
+    }
+}

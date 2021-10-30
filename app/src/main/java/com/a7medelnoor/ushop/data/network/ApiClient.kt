5b41db61fe59d04dd182e41d9fdb2b542e556bb2
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-30 13:03:07 +0400

    add interceptor and ok http client to our api client class

--- app/src/main/java/com/a7medelnoor/ushop/data/network/ApiClient.kt
+++ app/src/main/java/com/a7medelnoor/ushop/data/network/ApiClient.kt
@@ -1,18 +1,29 @@
 package com.a7medelnoor.ushop.data.network
 
 import com.a7medelnoor.ushop.util.AppConstants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
     private lateinit var uShopApiService: UShopApiService
 
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        this.level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client: OkHttpClient = OkHttpClient.Builder().apply {
+        this.addInterceptor(interceptor)
+    }.build()
 
     fun getApiService(): UShopApiService {
         // initialized api service if not initialized yet
-        if (uShopApiService != null) {
+        if (!::uShopApiService.isInitialized) {
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
+
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 

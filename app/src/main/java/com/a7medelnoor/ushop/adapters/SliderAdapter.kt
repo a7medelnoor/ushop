8ecd52527201e7be68f531841d8767cebb130c9e
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-10-30 15:37:45 +0400

    create slider adapter and setup our slider item

--- app/src/main/java/com/a7medelnoor/ushop/adapters/SliderAdapter.kt
+++ app/src/main/java/com/a7medelnoor/ushop/adapters/SliderAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.a7medelnoor.ushop.R
-import com.a7medelnoor.ushop.data.model.ImageSlider
+import com.a7medelnoor.ushop.data.model.dto.ImageSlider
 import com.bumptech.glide.Glide
 import com.smarteist.autoimageslider.SliderViewAdapter
 

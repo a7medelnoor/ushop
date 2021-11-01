8ac9653d29f8cd9610393d69496ec6749b9684f8
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-11-02 00:31:10 +0400

    add subcategories list

--- app/src/main/java/com/a7medelnoor/ushop/data/model/response/SubCategoriesData.kt
+++ app/src/main/java/com/a7medelnoor/ushop/data/model/response/SubCategoriesData.kt
@@ -2,13 +2,13 @@ package com.a7medelnoor.ushop.data.model.response
 
 import com.google.gson.annotations.SerializedName
 
-data class CategoriesData(
+data class SubCategoriesData (
     @SerializedName("id")
-     val id: Int,
+    val id: Int,
     @SerializedName("name")
-     val name: String,
+    val name: String,
     @SerializedName("image")
-     val image: String,
+    val image: String,
     @SerializedName("subcategories")
-     val subcategories: List<SubCategoriesData>
-)
+    val subcategories: List<SubCategoriesData>
+        )
